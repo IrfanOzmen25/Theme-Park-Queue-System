2441305b997956f8c9fe80d5d751ce1cdc79deae
IrfanOzmen25 <108149311+IrfanOzmen25@users.noreply.github.com>
2024-07-11 02:22:41 -0500

    Delete Homework4 directory

--- Homework4/src/Status.java
+++
@@ -1,7 +0,0 @@
-/**
- * The Status enum represents the possible statuses of a person in a theme park system.
- * It includes three states: Available, Holding, and OnRide.
- */
-public enum Status {
-    Available, Holding, OnRide
-}
